de0e92d3b3fcde51d76078c56e0e4ccedf84b6d3
kiki1214 <82639938+kiki1214@users.noreply.github.com>
2021-04-19 09:47:16 +0800

    Update Ssmdemo002Application.java

--- src/main/java/com/yidu/ssmdemo002/Ssmdemo002Application.java
+++ src/main/java/com/yidu/ssmdemo002/Ssmdemo002Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ssmdemo002Application {
 
     public static void main(String[] args) {
-        System.out.println("helloWorld");
+        System.out.println("helloWorld---");
         SpringApplication.run(Ssmdemo002Application.class, args);
     }
 
